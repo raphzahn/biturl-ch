5afdb9bef3f8985d5bc44d427546b33ff1ee350e
Raphael Zahner <r.zahner@voegeli.ch>
2024-01-25 20:38:02 +0100

    add /url route

--- server/main.js
+++ server/main.js
@@ -1,8 +1,70 @@
 const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const yup = require('yup')
+const monk = require('monk')
+const { nanoid } = require('nanoid')
+
+require('dotenv').config();
+
+const db = monk(process.env.MONGODB_URI);
+const urls = db.get('urls');
+urls.createIndex({ slug: 1 }, { unique: true });
+
 const app = express()
 const port = process.env.PORT || 3000;
+const schema = yup.object().shape({
+  slug: yup.string().trim().matches(/^[\w\-]+$/i),
+  url: yup.string().trim().url().required()
+});
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.json());
 
 app.use(express.static('client'))
+
+
+app.post('/url', async(req, res, next) => {
+  let {slug, url} = req.body;
+  try {
+    await schema.validate({
+      slug,
+      url,
+    });
+    if (!slug) {
+      slug = nanoid(5);
+    } else {
+      const existing = await urls.findOne({ slug });
+      if (existing) {
+        throw new Error('Slug in use. 🍔');
+      }
+    }
+    slug = slug.toLowerCase();
+    const newUrl = {
+      url,
+      slug,
+    };
+    const created = await urls.insert(newUrl);
+    res.json(created);
+  } catch (error) {
+    next(error);
+  }
+})
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
